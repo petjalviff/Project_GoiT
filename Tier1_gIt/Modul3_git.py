0b7fb0ef6e0e36512e34881ae35ca54a3e40a8bd
Andrii Petryshyn <petjalviff@gmail.com>
2024-02-04 16:22:29 +0200

    Added UA language

--- Tier1_gIt/Modul3_git.py
+++ Tier1_gIt/Modul3_git.py
@@ -22,9 +22,33 @@ def hello_en():
     except IndentationError:
         print("You enterned not Integer!")
         b=int(input("Enter years old: "))
+def hello_ua():
+    print("****************")
+    print("Привіт")
+    print("Як тебе звати?")
+    a=input("Введи своє імя: ")
+    print(f"Привіт, {a}, скільки тобі років?")
+    b=int(input("Введи свій вік: "))
+    try:
+        if b<c:
+            d=c-b
+            print(f"Я на {d} роки(ів) старший від тебе, {a}" )
+        else:
+            if b==c:
+                print("Ми з тобою одного віку")
+            else: d=b-c
+            print(f"я на {d} роки(ів) молодший, {a}")
+    
+    except ValueError:
+        print("You enterned not Integer!")
+        b=int(input("Enter years old: "))
+    except IndentationError:
+        print("You enterned not Integer!")
+        b=int(input("Enter years old: "))
 while True:
     leng=input("Enter lenguage (en for English or ua for Ucrainian): ")
-    if leng=="en":
-        hello_en()
-    else: print("Вибачте даною мовою не підтримується, ми скоро виправимо це.")
+    if leng=="ua":
+        hello_ua()
+    else: hello_en()
     print("Godbye")
+    print("*******************")
