f1df6e330aa1b55e415eabd509b6336d125b0e4c
Igor <petjalviff@gmail.com>
2024-03-09 19:51:03 +0200

    M1-_HW6

--- Tier1_gIt/modul_10/modul_10.py
+++ Tier1_gIt/modul_10/modul_10.py
@@ -77,7 +77,63 @@
 # print(first_animal.color)
 # print(second_animal.color)
 
+# *********************<<<<<<< Homework 5 >>>>>>>>>>****************
+# -------> theory
+# -----> Homework
 
+# class Animal:
+#     def __init__(self, nickname, weight):
+#         self.nickname = nickname
+#         self.weight = weight
+
+#     def say(self):
+#         pass
+
+#     def change_weight(self, weight):
+#         self.weight = weight
+
+
+# class Cat(Animal):
+#     def say(self):
+#         return "Meow"
+    
+
+# cat=Cat("Simon", 10)
+
+# print(cat.say())
+
+
+# *********************<<<<<<< Homework 6 >>>>>>>>>>****************
+# -------> theory
+# -----> Homework
+
+class Animal:
+    def __init__(self, nickname, weight):
+        self.nickname = nickname
+        self.weight = weight
+
+    def say(self):
+        pass
+
+    def change_weight(self, weight):
+        self.weight = weight
+
+
+class Dog(Animal):
+    def __init__(self, nickname, weight, breed):
+        super().__init__(nickname, weight)
+        self.breed=breed
+        
+    def say(self):
+        return "Woof"
+    
+
+dog = Dog("Barbos", 23, "labrador")
+
+print(dog.nickname)
+print(dog.weight)
+print(dog.breed)
+print(dog.say())
 
 
 # *********************<<<<<<< Homework 8 >>>>>>>>>>****************
