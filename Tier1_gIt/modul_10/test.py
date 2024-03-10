bd5f21ad0fb9b52e3fa25e4a576d77a3b8f9ae74
Andrii Petryshyn <petjalviff@gmail.com>
2024-03-10 16:16:51 +0200

    dfgh

--- Tier1_gIt/modul_10/test.py
+++ Tier1_gIt/modul_10/test.py
@@ -1,69 +1,69 @@
-from collections import UserDict
+# from collections import UserDict
 
-class Field: 
-    def __init__(self, value):
-        self.value = value
+# class Field: 
+#     def __init__(self, value):
+#         self.value = value
 
-    def __str__(self):
-        return str(self.value)
+#     def __str__(self):
+#         return str(self.value)
 
-class Name(Field):
-    pass
+# class Name(Field):
+#     pass
 
-class Phone(Field):
-    def __init__(self, phone):
-        if len(phone) >= 10 :
-            super().__init__(phone)
-        else:
-            raise ValueError
+# class Phone(Field):
+#     def __init__(self, phone):
+#         if len(phone) >= 10 :
+#             super().__init__(phone)
+#         else:
+#             raise ValueError
 
-class Record:
-    def __init__(self, name_r):
-        self.name = Name(name_r)
-        self.phones = [] # [Phone('1234222')]
+# class Record:
+#     def __init__(self, name_r):
+#         self.name = Name(name_r)
+#         self.phones = [] # [Phone('1234222')]
 
-    # реалізація класу
-    def find_phone(self, phone):
-        for user_phone in self.phones:
-            if user_phone.value == phone:
-                print("find user_phone is", user_phone)
-                return user_phone
+#     # реалізація класу
+#     def find_phone(self, phone):
+#         for user_phone in self.phones:
+#             if user_phone.value == phone:
+#                 print("find user_phone is", user_phone)
+#                 return user_phone
 
-    def add_phone(self, phone_r):
-        
-    
-    
-    def __str__(self):
-        return f"Contact name: {self.name.value}, phones: {'; '.join(p.value for p in self.phones)}"
-
-class AddressBook(UserDict):
-    def __int__(self):
-        self.data = []
-    # def add_record(self, name, phone)
-    def add_record(self, record):
-        # name = record.name.value
-        self.data = record
-    
-    def all_book(self):
-        return str(self.data)
+#     def add_phone(self, phone_r):
 
     
-record = Record('Oleg')
-record.phones.append(Phone('1234453568'))
-print(record)
+    
+#     def __str__(self):
+#         return f"Contact name: {self.name.value}, phones: {'; '.join(p.value for p in self.phones)}"
 
-phone_find = record.find_phone('1234453568')
-print(phone_find)
-phone_find.value = '0987654335'
-print(record)
+# class AddressBook(UserDict):
+#     def __int__(self):
+#         self.data = []
+#     # def add_record(self, name, phone)
+#     def add_record(self, record):
+#         # name = record.name.value
+#         self.data = record
+    
+#     def all_book(self):
+#         return str(self.data)
 
-addressBook = AddressBook()
-addressBook.add_record(record)
-print(addressBook.all_book())
+    
+# record = Record('Oleg')
+# record.phones.append(Phone('1234453568'))
+# print(record)
 
-record2=Record("Andrii")
-addressBook.add_record(record2)
-print(addressBook.all_book())
+# phone_find = record.find_phone('1234453568')
+# print(phone_find)
+# phone_find.value = '0987654335'
+# print(record)
+
+# addressBook = AddressBook()
+# addressBook.add_record(record)
+# print(addressBook.all_book())
+
+# record2=Record("Andrii")
+# addressBook.add_record(record2)
+# print(addressBook.all_book())
 
 
 
