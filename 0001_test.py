9037e385262f57150c92e89ecc545f91d04056fd
Andrii Petryshyn <petjalviff@gmail.com>
2024-04-28 17:17:46 +0300

    123

--- 0001_test.py
+++ 0001_test.py
@@ -1,23 +1,146 @@
-
-def repeat(num_times):
-    def decorator_repeat(func):
-        def wrapper(*args, **kwargs):
-            for _ in range(num_times):
-                result = func(*args, **kwargs)
-            return result
-        return wrapper
-    return decorator_repeat
-
-# Параметризований декоратор
-@repeat(num_times=3)
-def greet(name):
-    print(f"Hello {name}")
-
-# Виклик декорованої функції
-greet("World")
-d="Єілдвопаір іволдпідп"
+# class Node:
+#     def __init__(self, data):
+#         self.data = data
+#         self.next = None
+#         self.prev = None
+    
+#     def __str__(self):
+#         return str(self.data)
 
 
-from collections import UserList
+# class DoublyLinkedList:
+#     def __init__(self):
+#         self.head = None
+#         self.tail = None
 
-class Gdkfj(UserList)
+#     # Додавання вузла на кінець списку
+#     def append(self, data):
+#         new_node = Node(data)
+#         if self.head is None:
+#             self.head = new_node
+#             self.tail = self.head
+#         else:
+#             self.tail.next = new_node
+#             new_node.prev = self.tail
+#             self.tail = new_node
+
+#     # Додавання вузла на початок списку
+#     def push(self, data):
+#         new_node = Node(data)
+#         new_node.next = self.head
+#         if self.head:
+#             self.head.prev = new_node
+#         else:
+#             self.tail = new_node
+#             self.head = new_node
+
+#     # Додавання вузла після заданого вузла
+#     def insert_after(self, prev_node, data):
+#         if not prev_node:
+#             return
+#         new_node = Node(data)
+#         new_node.next = prev_node.next
+#         prev_node.next = new_node
+#         new_node.prev = prev_node
+#         if new_node.next:
+#             new_node.next.prev = new_node
+#         else:
+#             self.tail = new_node
+
+#     # Додавання вузла перед заданим вузлом
+#     def insert_before(self, next_node, data):
+#         if not next_node:
+#             return
+#         new_node = Node(data)
+#         new_node.prev = next_node.prev
+#         next_node.prev = new_node
+#         new_node.next = next_node
+#         if new_node.prev:
+#             new_node.prev.next = new_node
+#         else:
+#             self.head = new_node
+
+#     def remove(self, data):
+#         current_node = self.head
+#         while current_node:
+#             if current_node.data == data:
+#                 if current_node.prev:
+#                     current_node.prev.next = current_node.next
+#                 else:
+#                     self.head = current_node.next
+#                 if current_node.next:
+#                     current_node.next.prev = current_node.prev
+#                 else:
+#                     self.tail = current_node.prev
+#                 current_node.prev = None
+#                 current_node.next = None
+#             return True
+#         current_node = current_node.next
+#         return False
+
+#     def search(self, target_data):
+#         current_node = self.head
+#         while current_node:
+#             if current_node.data == target_data:
+#                 return current_node
+#             current_node = current_node.next
+#         return None
+
+# ddl=DoublyLinkedList()
+
+# ddl.append(1)
+# ddl.append("Andrii")
+
+# nnd=Node
+
+# print(nnd)
+
+
+# *************************************Hash Tags
+
+# my_dict = {}
+# my_dict["key1"] = "value1"
+# my_dict["key2"] = "value2"
+# print(my_dict)  # {'key1': 'value1', 'key2': 'value2'}
+
+# del my_dict["key1"]
+
+# print(my_dict)
+
+# my_dict["hash"]="Andrii"
+
+# print(my_dict)
+# v=my_dict.get("key2")
+# print(v)
+
+
+
+#****************************Graphs
+
+import networkx as nx
+import matplotlib.pyplot as plt
+
+# Створюємо порожній граф
+G = nx.Graph()
+
+# Додаємо вершини
+G.add_node("A")
+G.add_node("B")
+G.add_node("C")
+
+# Додаємо з'єднання
+G.add_edge("A", "B")
+G.add_edge("B", "C")
+
+# Задаємо розташування вершин
+positions = {
+    "A": (0, 2.5),
+    "B": (-1, 4.5),
+    "C": (2, 0.5)
+}
+
+# Малюємо граф
+plt.figure(figsize=(6,6))
+nx.draw_networkx(G, pos=positions, with_labels=True, font_weight='bold', node_color='lightblue', edge_color='gray')
+plt.axis("on")
+plt.show()
---
+++ Tier1_mathematics/modul_9/01.py
@@ -0,0 +1,114 @@
+# import numpy as np
+# import matplotlib.pyplot as plt
+# import matplotlib
+
+
+# matplotlib.rcParams.update({'font.size': 10})
+
+
+# x = np.linspace(0, 10, 100)
+# # x1 = []
+# # for i in range(100):
+# #     x1.append(3)
+# #     print(x1)
+# # a=len(x1)
+# # print("a=",a)
+# # y1 = np.linspace(0, 10, 100)
+# f1 = 2*x
+# f2 = (1/4)*x
+# f3 = 1-x
+
+
+
+# # вказуємо в аргументі label текст легенди
+# plt.plot(x, f1, ':b', label='x2>2x1')
+# plt.plot(x, f2, '--b', label='x2<1/4x1')
+# plt.plot(x, f3, 'y', label='x2>1-x1')
+
+
+
+# plt.xlabel(r'$x2$', fontsize=10)
+# plt.ylabel(r'$x3$', fontsize=10)
+
+
+# plt.xlim([0, 3])
+# plt.ylim([0, 3])
+
+
+# # виводимо легенду
+# plt.legend(fontsize=10)
+
+
+# # plt.tight_layout()
+# plt.show()
+
+
+#**********************************************************************************
+# import numpy as np
+# import matplotlib.pyplot as plt
+# import matplotlib
+
+
+# matplotlib.rcParams.update({'font.size': 12})
+
+
+# x = np.linspace(0, 10, 100)
+
+
+# f1 = 2*x
+# f2 = (1/3)*x
+# f3 = 1-x
+# f4 = 5/2-(1/2)*x
+# ff = 0.5*x
+
+
+# # вказуємо в аргументі label текст легенди
+# plt.plot(x, f1, ':b', label='x2<2x1')
+# plt.plot(x, f2, 'k', label='x2>1/3x1')
+# plt.plot(x, f3, '--b', label='x2>1-x1')
+# plt.plot(x, f4, '--g', label='x2<5/2-1/2x1')
+
+
+# plt.xlabel(r'$x1$', fontsize=16)
+# plt.ylabel(r'$x2$', fontsize=16)
+
+
+# plt.xlim([0, 3])
+# plt.ylim([0, 3])
+
+
+# # виводимо легенду
+# plt.legend(fontsize=14)
+
+
+# plt.tight_layout()
+# plt.show()
+
+#***************************************************************************************
+import time
+
+
+from scipy.optimize import linprog
+
+
+start = time.time()
+
+
+c = [-1, -2] # Цільова функція — на максимум (змінили знак)
+A_ub = [
+    [-2, 1],
+    [1, -3],
+    [-1, -1],
+    [1, 2],
+    [-1, 0],
+    [0, -1],
+] # Коефіцієнти при нерівностях
+b_ub = [0,0,-1,5,0,0] # Результати при нерівностях
+
+
+print(linprog(c, A_ub, b_ub)) # Оптимізація
+
+
+stop = time.time()
+print("Час :")
+print(stop - start)
