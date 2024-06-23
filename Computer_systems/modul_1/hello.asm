b3af3bd2c138fcdbc66124c4a356f3bae62a86ea
Andrii Petryshyn <petjalviff@gmail.com>
2024-06-23 19:02:50 +0300

    111

--- Computer_systems/modul_1/hello.asm
+++ Computer_systems/modul_1/hello.asm
@@ -1,1 +1,13 @@
-ghkghj, 
+org  0x100             ; Вказівка, що це програма .COM
+section .data
+    message db 'Hello, World!', '$' ; Визначення рядка для виведення
+
+section .text
+_start:
+    mov ah, 09h        ; Функція DOS для виведення рядка
+    mov dx, message    ; Встановити DX на адресу рядка
+    int 21h            ; Виклик DOS-переривання
+
+    mov ax, 4c00h      ; Функція DOS для завершення програми
+    int 21h            ; Виклик DOS-переривання
+
